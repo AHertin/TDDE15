046c545eb93743e04e0a28ffa1d7f0835d222241
Andreas Hertin <andhe794@student.liu.se>
2021-09-09 13:55:59 +0200

    Code for Lab 1, task 2

--- task_1.R
+++ task_1.R
@@ -8,7 +8,7 @@
 
 ## Install if not installed
 ## if (!requireNamespace("BiocManager", quietly = TRUE))
-##   install.packages("BiocManager")
+##  install.packages("BiocManager")
 ## BiocManager::install("RBGL")
 ## BiocManager::install("Rgraphviz")
 ## BiocManager::install("gRain")
@@ -16,7 +16,8 @@
 ## Import packages
 ## library(RGBL)
 ## library(Rgraphviz)
-## library(gRain)
+library(gRain)
+library(bnlearn)
 
 ## Import data
 data("asia")
@@ -27,23 +28,29 @@ data("asia")
 ## Task 1
 
 set.seed(1)
+HC_0 <- hc(x = asia)
+
+set.seed(1)
 HC_1 <- hc(x = asia,
            score = "bde",
            restart = 3)
-plot(HC_1)
+# plot(HC_1)
 
 set.seed(1)
 HC_2 <- hc(x = asia,
            score = "bde",
            restart = 1000)
-plot(HC_2)
+# plot(HC_2)
 
-all.equal(HC_1, HC_2)
-
+set.seed(1)
 HC_3 <- hc(x = asia,
            score = "bic",
            restart = 1000)
-## plot(HC_2)
+# plot(HC_2)
+
+all.equal(HC_0, HC_1)
+all.equal(HC_0, HC_2)
+all.equal(HC_0, HC_3)
 
 vstructs(HC_1)
 vstructs(HC_2)
@@ -53,10 +60,94 @@ arcs(HC_1)
 arcs(HC_2)
 arcs(HC_3)
 
-cpdag(HC_0)
 cpdag(HC_1)
 cpdag(HC_2)
 cpdag(HC_3)
+
+################################################################################
+## Task 2
+predictNet <- function(juncTree, data, features, target){
+  predArray <- matrix(nrow = nrow(data),
+                      ncol = 1)
+  
+  for(i in 1:nrow(data)){
+    obsStates <- NULL
+    for(p in features){
+      if(data[i,p] == "yes"){
+        obsStates <- c(obsStates,"yes")
+      } else{
+        obsStates <- c(obsStates,"no")
+      }
+    }
+    
+    
+    obsEvidence <- setEvidence(object = juncTree,
+                               nodes = features,
+                               states = obsStates)
+    
+    obsPredProb <- querygrain(object = obsEvidence,
+                              nodes = target)$S
+    
+    predArray[i] <- if(obsPredProb["yes"] >= 0.5) "yes" else "no"
+  }
+  
+  return(predArray)
+  
+}
+
+
+trainingIndices = sample(1:dim(asia)[1],
+                         floor(dim(asia)[1] * 0.8))
+
+asia.train <- asia[trainingIndices,]
+asia.test <- asia[-trainingIndices,]
+
+set.seed(1)
+HC_BN_test <- hc(x = asia.train,
+                 score = "bde",
+                 restart = 3)
+
+HC_BN_true <- model2network("[A][S][T|A][L|S][B|S][D|B:E][E|T:L][X|E]")
+
+# plot(HC_BN_test)
+# plot(HC_BN_true)
+
+fitTest <- bn.fit(HC_BN_test,
+                  asia.train)
+
+fitTrue <- bn.fit(HC_BN_true,
+                  asia.train)
+
+fitTestTable = as.grain(fitTest)
+fitTrueTable = as.grain(fitTrue)
+
+fitTestJunctionTree = compile(fitTestTable)
+fitTrueJunctionTree = compile(fitTrueTable)
+
+observedVars = c("A", "T", "L", "B", "E", "X", "D")
+targetVar   = c("S")
+
+predTest <- predictNet(fitTestJunctionTree,
+                       asia.test,
+                       observedVars,
+                       targetVar)
+
+predTrue <- predictNet(fitTestJunctionTree,
+                      asia.test,
+                      observedVars,
+                      targetVar)
+
+confMatrixTest <- table(predTest, asia.test$S)
+confMatrixTrue <- table(predTrue, asia.test$S)
+
+print(confMatrixTest)
+print(confMatrixTrue)
+
+
+
+
+
+
 
 
 
